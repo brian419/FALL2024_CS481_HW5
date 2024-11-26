6ce73fb0a1d45fd328bc242acbe0c84ccace34aa
brian419 <json10@crimson.ua.edu>
2024-11-26 08:48:19 -0600

    not sure which gpu arch i can use

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-#PBS -N game_of_life_mpi_job
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=10:00:00
-#PBS -l mem=4gb
+#PBS -N gpu_test
+#PBS -l nodes=1:ppn=1:gpus=1
+#PBS -l walltime=00:05:00
 #PBS -q gpu
-#PBS -o /scratch/ualclsd0197/output_dir/game_of_life_output.txt
-#PBS -e /scratch/ualclsd0197/output_dir/game_of_life_error.txt
+#PBS -o gpu_test_output.txt
+#PBS -e gpu_test_error.txt
 
 cd $PBS_O_WORKDIR
 
 module load cuda
-nvcc -o gameoflife gameoflife.cu
-
-./gameoflife 10000 10000
-
+nvidia-smi
