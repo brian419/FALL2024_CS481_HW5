209d9047e4638652c836b5ab92e3d162956d21a7
brian419 <json10@crimson.ua.edu>
2024-11-26 08:44:11 -0600

    reverting

--- run_gpu.sh
+++ run_gpu.sh
@@ -12,10 +12,5 @@ cd $PBS_O_WORKDIR
 module load cuda
 nvcc -o gameoflife gameoflife.cu
 
-# ./gameoflife 10000 10000
-
-
-
-./gameoflife 10000 10000 > /scratch/ualclsd0197/output_dir/game_of_life_output.txt 2>&1
-
+./gameoflife 10000 10000
 
