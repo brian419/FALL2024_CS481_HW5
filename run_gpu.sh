75092bda3056334b9b9a258a06996768912f1ae3
brian419 <json10@crimson.ua.edu>
2024-11-26 08:37:27 -0600

    attempting to use gpu

--- run_gpu.sh
+++ run_gpu.sh
@@ -3,7 +3,7 @@
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=10:00:00
 #PBS -l mem=4gb
-#PBS -q class
+#PBS -q gpu
 #PBS -o /scratch/ualclsd0197/output_dir/game_of_life_output.txt
 #PBS -e /scratch/ualclsd0197/output_dir/game_of_life_error.txt
 
