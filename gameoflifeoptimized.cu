cc0b27d9fa47da2e484754f38c9866f8f40fc927
brian419 <json10@crimson.ua.edu>
2024-11-29 14:10:53 -0600

    updating code

--- gameoflifeoptimized.cu
+++ gameoflifeoptimized.cu
@@ -3,10 +3,8 @@
 #include <cstdlib>
 #include <chrono>
 #include <fstream>
-
 using namespace std;
 using namespace std::chrono;
-
 #define CHECK_CUDA_ERROR(err) \
     { \
         if (err != cudaSuccess) { \
@@ -14,129 +12,101 @@ using namespace std::chrono;
             exit(1); \
         } \
     }
-
-__global__ void gameOfLifeKernelOptimized(int *current, int *next, int boardSize) {
-    extern __shared__ int sharedBoard[];
-    int localRow = threadIdx.y + 1;
-    int localCol = threadIdx.x + 1;
+__global__ void gameOfLifeKernel(const int *__restrict__ current, int *next, int boardSize) {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-
-    int globalIdx = row * boardSize + col;
-    int bitIdx = globalIdx % 32;
-    int arrayIdx = globalIdx / 32;
-
     if (row < boardSize && col < boardSize) {
-        sharedBoard[localRow * (blockDim.x + 2) + localCol] = (current[arrayIdx] >> bitIdx) & 1;
-
-        if (threadIdx.x == 0 && col > 0)
-            sharedBoard[localRow * (blockDim.x + 2)] = (current[(arrayIdx - 1)] >> (31)) & 1;
-        if (threadIdx.x == blockDim.x - 1 && col < boardSize - 1)
-            sharedBoard[localRow * (blockDim.x + 2) + blockDim.x + 1] = (current[(arrayIdx + 1)] & 1);
-        if (threadIdx.y == 0 && row > 0)
-            sharedBoard[(localRow - 1) * (blockDim.x + 2) + localCol] = (current[arrayIdx - boardSize / 32] >> bitIdx) & 1;
-        if (threadIdx.y == blockDim.y - 1 && row < boardSize - 1)
-            sharedBoard[(localRow + 1) * (blockDim.x + 2) + localCol] = (current[arrayIdx + boardSize / 32] >> bitIdx) & 1;
-
-        __syncthreads();
-
+        int index = row * boardSize + col;
+        int up = row - 1;
+        int down = row + 1;
+        int left = col - 1;
+        int right = col + 1;
         int aliveNeighbors = 0;
-        for (int i = -1; i <= 1; ++i) {
-            for (int j = -1; j <= 1; ++j) {
-                if (i == 0 && j == 0) continue;
-                aliveNeighbors += sharedBoard[(localRow + i) * (blockDim.x + 2) + (localCol + j)];
-            }
-        }
-
-        int currentCellState = sharedBoard[localRow * (blockDim.x + 2) + localCol];
-        int newState = (currentCellState == 1)
-                           ? (aliveNeighbors < 2 || aliveNeighbors > 3 ? 0 : 1)
-                           : (aliveNeighbors == 3 ? 1 : 0);
-
-        atomicOr(&next[arrayIdx], newState << bitIdx);
+        if (up >= 0 && left >= 0)
+            aliveNeighbors += __ldg(&current[up * boardSize + left]);
+        if (up >= 0)
+            aliveNeighbors += __ldg(&current[up * boardSize + col]);
+        if (up >= 0 && right < boardSize)
+            aliveNeighbors += __ldg(&current[up * boardSize + right]);
+        if (left >= 0)
+            aliveNeighbors += __ldg(&current[row * boardSize + left]);
+        if (right < boardSize)
+            aliveNeighbors += __ldg(&current[row * boardSize + right]);
+        if (down < boardSize && left >= 0)
+            aliveNeighbors += __ldg(&current[down * boardSize + left]);
+        if (down < boardSize)
+            aliveNeighbors += __ldg(&current[down * boardSize + col]);
+        if (down < boardSize && right < boardSize)
+            aliveNeighbors += __ldg(&current[down * boardSize + right]);
+        int cellState = __ldg(&current[index]);
+        next[index] = (cellState == 1) ? (aliveNeighbors < 2 || aliveNeighbors > 3 ? 0 : 1) : (aliveNeighbors == 3 ? 1 : 0);
     }
 }
-
-void initializeBoardBitPacked(int *board, int boardSize) {
+void initializeBoard(int *board, int boardSize) {
     srand(12345);
     for (int i = 0; i < boardSize * boardSize; ++i) {
-        int bitIndex = i % 32;
-        int arrayIndex = i / 32;
-        if (bitIndex == 0) board[arrayIndex] = 0;
-        board[arrayIndex] |= ((rand() % 2) << bitIndex);
+        board[i] = rand() % 2;
     }
 }
-
-void writeFinalBoardToFile(const int *board, int n, int iterations, const string &outputDir) {
+void writeFinalBoardToFile(const int *board, int n, int iterations, const string &outputDir)
+{
     string correctedOutputDir = outputDir;
     if (outputDir.back() != '/')
+    {
         correctedOutputDir += "/";
+    }
     string fileName = correctedOutputDir + "hw5_GPU_" + to_string(n) + "x" + to_string(n) +
                       "_board_" + to_string(iterations) + "_iterations_V3code_Optimized_testcase.txt";
     ofstream outFile(fileName);
     if (!outFile)
+    {
+        printf("Error creating output file: %s\n", fileName.c_str());
         return;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            int globalIdx = i * n + j;
-            int bitIdx = globalIdx % 32;
-            int arrayIdx = globalIdx / 32;
-            outFile << (((board[arrayIdx] >> bitIdx) & 1) ? '*' : '.') << " ";
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            outFile << (board[i * n + j] ? '*' : '.') << " ";
         }
         outFile << endl;
     }
     outFile.close();
+    printf("Final board written to %s\n", fileName.c_str());
 }
-
 int main(int argc, char *argv[]) {
     if (argc != 4) {
         cout << "Usage: " << argv[0] << " <board size> <generations> <output directory>" << endl;
         return 1;
     }
-
     int boardSize = stoi(argv[1]);
     int generations = stoi(argv[2]);
     string outputDir = argv[3];
-
-    size_t numPackedInts = (boardSize * boardSize + 31) / 32;
-    size_t size = numPackedInts * sizeof(int);
-    int *h_current = new int[numPackedInts];
-    int *h_next = new int[numPackedInts];
-
-    initializeBoardBitPacked(h_current, boardSize);
-
+    size_t size = boardSize * boardSize * sizeof(int);
+    int *h_current = new int[boardSize * boardSize];
+    int *h_next = new int[boardSize * boardSize];
+    initializeBoard(h_current, boardSize);
     int *d_current, *d_next;
     CHECK_CUDA_ERROR(cudaMalloc(&d_current, size));
     CHECK_CUDA_ERROR(cudaMalloc(&d_next, size));
     CHECK_CUDA_ERROR(cudaMemcpy(d_current, h_current, size, cudaMemcpyHostToDevice));
-
     dim3 threadsPerBlock(16, 16);
     dim3 blocksPerGrid((boardSize + threadsPerBlock.x - 1) / threadsPerBlock.x,
                        (boardSize + threadsPerBlock.y - 1) / threadsPerBlock.y);
-
-    size_t sharedMemorySize = (threadsPerBlock.x + 2) * (threadsPerBlock.y + 2) * sizeof(int);
-
     auto start = high_resolution_clock::now();
-
     for (int gen = 0; gen < generations; ++gen) {
-        gameOfLifeKernelOptimized<<<blocksPerGrid, threadsPerBlock, sharedMemorySize>>>(d_current, d_next, boardSize);
+        gameOfLifeKernel<<<blocksPerGrid, threadsPerBlock>>>(d_current, d_next, boardSize);
         CHECK_CUDA_ERROR(cudaGetLastError());
         CHECK_CUDA_ERROR(cudaMemcpy(d_current, d_next, size, cudaMemcpyDeviceToDevice));
     }
-
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(end - start);
-
     CHECK_CUDA_ERROR(cudaMemcpy(h_next, d_current, size, cudaMemcpyDeviceToHost));
-
     writeFinalBoardToFile(h_next, boardSize, generations, outputDir);
-
     cout << "Simulation completed in " << duration.count() << " ms." << endl;
-
     cudaFree(d_current);
     cudaFree(d_next);
     delete[] h_current;
     delete[] h_next;
-
     return 0;
 }
